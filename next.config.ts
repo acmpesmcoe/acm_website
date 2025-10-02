bd4714cde0e86fc8f5d57d446f10b4c43fc11a13
SohamUser <sohamkulakrni@gmail.com>
2025-10-02 16:22:46 +0530

    complete website images-3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/tw7e6u1ae/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
